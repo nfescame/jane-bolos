e8fb3332613fcd3d62a23ee97082d5517e8f368e
nilton <nfescame@gmail.comq:qclear>
2021-11-19 11:55:14 -0300

    request delete

--- src/page/adm/requests.jsx
+++ src/page/adm/requests.jsx
@@ -22,6 +22,18 @@ export default function Requests() {
     })();
   }, [dataProvider]);
 
+  async function deleteItem(event) {
+    const id = await event;
+    try {
+      const result = await api.delete(`/request/delete/${id}`);
+      console.log(result.data);
+      // eslint-disable-next-line no-restricted-globals
+      location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className='container' style={{ paddingTop: "5rem" }}>
       <div className='container-btn'>
@@ -43,12 +55,28 @@ export default function Requests() {
             }}
           >
             <div className='card-body'>
+              <p>
+                Feito:
+                <input
+                  className='form-check-input m-1'
+                  type='checkbox'
+                  defaultChecked={req.done}
+                  aria-label='Checkbox for following text input'
+                />
+              </p>
+
               <h5 className='card-title'>{req.clientName}</h5>
               <p className='card-text'>{req._id}</p>
               <p className='card-text'>{req.totalValue}</p>
+
               <Link to={`/details/${req._id}`} className='btn btn-primary'>
                 Detalhar pedido
               </Link>
+              <div className=''>
+                <div onClick={() => deleteItem(req._id)}>
+                  <i className='fas fa-trash-alt'></i>
+                </div>
+              </div>
             </div>
           </div>
         );
